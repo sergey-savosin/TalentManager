b6bce11a9d9379ef9c4b6f21b21b35ef66b4c4a6
Sergey Savosin <savosin_sergey@mail.ru>
2017-05-20 17:29:35 +0300

    Добавьте файлы проекта.

---
+++ TalentManager.Data/Context.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalentManager.Data.Configuration;
+using TalentManager.Domain;
+
+namespace TalentManager.Data
+{
+    public class Context : DbContext
+    {
+        public Context() : base("DefaultConnection") { }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Configurations
+                .Add(new EmployeeConfiguration())
+                .Add(new DepartmentConfiguration());
+        }
+    }
+}
